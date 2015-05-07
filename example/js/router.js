1ea9c98f04cbea664282209d2f48b9c36b966164
Yauheni Kokatau <ekokotov@gmail.com>
2015-05-07 17:24:44 +0300

    add landing page with samples

--- example/js/router.js
+++ example/js/router.js
@@ -2,7 +2,7 @@ angular.module('test').config(function ($routeProvider) {
        $routeProvider.when('/:template', {
             controller:'mainController',
             templateUrl:function(params){
-                return !!params.template ? 'example/partials/'+ params.template + '.html' : "example/partials/basic.html";
+                return !!params.template ? 'example/partials/'+ params.template + '.html' : "example/partials/attributes.html";
             }
         }).when('/',{
              controller:'mainController',
