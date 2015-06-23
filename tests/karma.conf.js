936e19bbf5d34dc43cb6b3c785ae91b22824d5ef
Yauheni Kokatau <ekokotov@gmail.com>
2015-06-23 17:12:41 +0300

    change test env. in NPM

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -53,7 +53,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    autoWatch: false,
 
 
     // start these browsers
@@ -63,6 +63,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   });
 };
